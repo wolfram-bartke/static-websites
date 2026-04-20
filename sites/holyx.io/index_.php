ac1ea7edad08e54a1cc46acb3f2297aa720a7b8a
Wolfram Bartke <45764297+wolfram-bartke@users.noreply.github.com>
2026-04-20 15:27:41 +0200

    add holyx.io

---
+++ sites/holyx.io/index_.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hdengine.io</title>
+</head>
+<body>
+    <h1>hdengine.io</h1>
+    <p>Server: <?php echo gethostname(); ?></p>
+    <p>PHP: <?php echo phpversion(); ?></p>
+    <p>Time: <?php echo date('Y-m-d H:i:s T'); ?></p>
+</body>
+</html>
